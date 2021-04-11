f2de03c4aec06a3c81068788eb973a69fea7d1cb
pseudo-su <stableford.john@gmail.com>
2021-04-12 00:13:45 +1000

    chore: the trinity

--- services/site-example-spa/README.md
+++ services/site-example-spa/README.md
@@ -1,3 +1,8 @@
 # website example using NextJS
 
 TODO
+
+## TODO list
+
+- Make CDK deployment work without `.html` suffix in pathnames (eg [https://stackoverflow.com/questions/23463679/s3-static-pages-without-html-extension](https://stackoverflow.com/questions/23463679/s3-static-pages-without-html-extension)).
+- Display NextJS 404 page whenever page not found.
--- services/site-example-spa/cdk/static-site.ts
+++ services/site-example-spa/cdk/static-site.ts
@@ -1,13 +1,39 @@
 import * as cdk from "@aws-cdk/core";
 import * as s3 from "@aws-cdk/aws-s3";
+import * as s3deploy from "@aws-cdk/aws-s3-deployment";
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
+import * as origins from "@aws-cdk/aws-cloudfront-origins";
 
 type StaticSiteStackProps = cdk.StackProps;
 
 export class StaticSiteStack extends cdk.Stack {
-  private bucket: s3.Bucket;
-
   constructor(scope: cdk.Construct, id: string, props: StaticSiteStackProps) {
     super(scope, id, props);
-    this.bucket = new s3.Bucket(this, `StaticSiteS3-${this.stackName}`);
+
+    const websiteBucket = new s3.Bucket(
+      this,
+      `WebsiteBucket-${this.stackName}`,
+      {
+        websiteIndexDocument: "index.html",
+        publicReadAccess: true,
+      },
+    );
+
+    // Handles buckets whether or not they are configured for website hosting.
+    const distribution = new cloudfront.Distribution(
+      this,
+      `Distribution-${this.stackName}`,
+      {
+        defaultBehavior: { origin: new origins.S3Origin(websiteBucket) },
+      },
+    );
+
+    new s3deploy.BucketDeployment(this, `DeployWebsite-${this.stackName}`, {
+      sources: [s3deploy.Source.asset("./out")],
+      destinationBucket: websiteBucket,
+      destinationKeyPrefix: "/",
+      distribution,
+      distributionPaths: ["/images/*.png"],
+    });
   }
 }
